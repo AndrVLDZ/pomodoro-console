0b41505cf019328c0b4c101baca00fb5336d10ab
Vladislav Andronov <68904879+AndrVLDZ@users.noreply.github.com>
2025-01-21 02:16:08 +0500

    Created a separate module for validation and other useful tools

---
+++ src/pomodoro/tools.py
@@ -0,0 +1,23 @@
+def get_positive_int(prompt: str) -> int:
+    """Prompt the user for a positive integer and return it."""
+    while True:
+        try:
+            value = int(input(prompt))
+            if value <= 0:
+                raise ValueError
+            return value
+        except ValueError:
+            print("Please enter a valid positive integer!")
+
+
+def get_yes_no(prompt: str, default: bool = False) -> bool:
+    """Prompt the user to enter 'Y' for Yes or 'N' for No and return the corresponding boolean value."""
+    while True:
+        default_text = "Yes" if default else "No"
+        answer = input(f"{prompt} [default is {default_text}]: ").strip().upper()
+        if answer in {"Y", "YES", "N", "NO"}:
+            return answer == "Y" or answer == "YES"
+        elif answer == "":
+            return default
+        else:
+            print("Please enter 'Y' for Yes or 'N' for No!")
